4a8d4ea288f506f65415ef7fa068231c60543c55
mayankbisht2112-stack <mayankbisht2112@gmail.com>
2025-10-23 04:09:25 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,18 @@
+let email = "mayank@gmail.com"
+let accountcity = "delhi"
+let accountstate;
+// in const we cant change the value but in let we can//
+const password = 12121
+// var email // we do not use var because of scope issue it is a function scope:
+/*
+
+if(true){
+var name="mayank"
+}
+console.log(name) */
+// even if we declare name inside the for loop it still accessable outside loop 
+// password=434343
+email = "hjhjhj@gmail.com"
+console.log(accountstate);
+console.log(email);
+console.table([email, accountcity, accountstate, password])
