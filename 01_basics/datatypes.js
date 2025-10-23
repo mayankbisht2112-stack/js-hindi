4b5822794175846d38e6e2cb1cd0bab3ac5b5396
mayankbisht2112-stack <mayankbisht2112@gmail.com>
2025-10-23 04:54:05 +0000

    dattype

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all code as JS newer version
+
+//alert(3+3)//it will give error because we are using node js not browser
+
+//dont ruin your code readability//
+
+//console.log(3+3); console.log("mayank")// not a good practice
+
+//datatypes//
+//number => 2 to power 53
+//bigint => for bigger numbers like used in trading apps, stock 
+//string => ""
+//boolean => true or false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+
+
+
+//object => talk about later
+ 
+console.log(typeof "mayank");
+console.log(typeof null);// object due to a historical implementation bug//
+console.log(typeof undefined);// undefined //
+
+
