5c1124b03cd81de8cbf411d7b867a89c9af1bae2
mayankbisht2112-stack <mayankbisht2112@gmail.com>
2025-10-24 04:54:56 +0000

    conversion&operator

---
+++ 01_basics/conversion.operation.js
@@ -0,0 +1,29 @@
+// let num = "33abc"
+// console.log(typeof num);
+// console.log(typeof (num));
+// let valueInNumber=Number(num);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//33 => 33
+//33abc => NaN (not a number)
+//true =>1
+//null =>0
+//undefined =>NaN
+
+let somenumber=33
+let valueInString=String(somenumber)
+// console.log(valueInString);
+// console.log(typeof valueInString);
+//we can convert diffrent datatypes to diff datatypes
+// why we use?
+// Because data often comes in as strings, but you might need numbers or booleans for operations.//
+
+
+// ***************Operations*************//
+let value=3
+let negValue=-value
+console.log(negValue);
+console.log("1"+2)
+console.log(1+2+"2");
