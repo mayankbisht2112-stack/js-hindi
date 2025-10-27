3ed4ede0b553ad3412b44bf2a4393b9fd33dd9cc
mayankbisht2112-stack <mayankbisht2112@gmail.com>
2025-10-27 05:49:56 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,33 @@
+//common comparision opeator//
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2<=1);
+
+
+//NOTE : 
+// console.log(null==0);//false
+// console.log(null>=0);//true
+// console.log(null>0);//false
+// The reason is that the equality check(==) and comparision(>=,>) work diffrently, comparisions convert null into num 0 //
+
+
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+//what we have to note here is that while comparing make sure that datatypes should be same
+
+
+
+
+
+
+
+
+
+
+
+
