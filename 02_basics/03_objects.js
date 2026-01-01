7a556646a0275d2fe1b5e18e725b44ab7977330a
mayankbisht2112-stack <mayankbisht2112@gmail.com>
2026-01-01 14:48:22 +0000

    OBJECTS

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -4,21 +4,24 @@
 const mysmb=Symbol("key1")
 const JSuser={
     //key// //value// 
-    name: "mayank",  // here system treate key(name) as a string
+    name: "mayank",  // here system treat key(name) as a string
     [mysmb]: "mykey",
     age: 19,
     "full name": "Mayank Bisht",
     email: "mayank@google.com",
     isLoggedIn: true,
-}
+}//JavaScript is a dynamically typed language, so types are decided at runtime, not before.//
 // console.log(JSuser.email);
 // console.log(JSuser["email"]);
 // console.log(JSuser.full name);//can't access through dot 
 // Dot notation only works with valid identifiers (no spaces, no quotes).
 // console.log(JSuser["full name"]);
 // console.log(JSuser["mysmb"]);
-// console.log(typeof JSuser["mysmb"]);
-// Object.freeze(JSuser)//now you cant change values in jsuser//
+console.log(typeof JSuser["age"]);
+console.log(typeof JSuser["mysmb"]);
+console.log(typeof JSuser["isLoggedIn"]);
+
+// Object.freeze(JSuser)//now you cant change values in JSuser//
 JSuser.email="mayank@chatgpt.com"
 // console.log(JSuser);
 JSuser.greeting=function(){
