c004d149f1d0656fcb5d0f5ea832f0daad06093d
mayankbisht2112-stack <mayankbisht2112@gmail.com>
2026-01-05 13:32:59 +0000

    SCOPES

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,34 @@
+if(true){
+let a=10;
+const b=20;
+var c=30;
+}
+// console.log(a);//will give error
+// console.log(b);//because let and const both are block scopes they are not accessible outside the scope
+// console.log(c);//it will give output because its not thats why we dont use var
+
+
+// NESTED SCOPES//
+// function one(){
+//     const username="mayank";
+//     function two(){
+//         const website="Github";
+//         console.log(username);
+//     }
+    // console.log(website);
+    
+
+    // two()
+// }
+// one()
+ 
+// SAME HAPPENS IN IF ELSE //
+if(true){
+    const username="mayank"
+    if(username==="mayank"){
+        const website=" youtube"
+        console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
