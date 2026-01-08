31ad168c91b006a725a6b058bcd2c33e373c4c85
mayankbisht2112-stack <mayankbisht2112@gmail.com>
2026-01-08 15:00:26 +0000

    iterations

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+//while and do while loop//
+
+// let myArray=["flash","batman","superman"]
+// let arr=0;
+// while(arr<myArray.length){
+//     console.log(`the hero is ${myArray[arr]}`);
+//     arr=arr+1;
+// }
+
+let score=1
+do {
+    console.log(`score : ${score}`)
+    score++
+} while(score<=10)
