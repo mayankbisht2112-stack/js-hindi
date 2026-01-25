1c842241c5576a31ab45b95d13265f82e9477411
Mayank Bisht <mayankbisht2112@gmail.com>
2026-01-25 19:54:43 +0530

    project1

---
+++ projects/script.js
@@ -0,0 +1,9 @@
+const buttons=document.querySelectorAll('.button')
+const body=document.querySelector('body')
+buttons.forEach(function(b){
+    b.addEventListener('click',function(e){
+        if(e.target.id===e.target.id){
+            body.style.backgroundColor=e.target.id
+        }
+    })
+})
