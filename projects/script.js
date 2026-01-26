4fc4a88f8f3ff380a32a0348eff40221d9688f02
Mayank Bisht <mayankbisht2112@gmail.com>
2026-01-26 12:24:57 +0530

    color_scheme_switcher

--- projects/script.js
+++ projects/script.js
@@ -1,9 +1,19 @@
-const buttons=document.querySelectorAll('.button')
+const buttons=document.querySelectorAll('.button') // we are storing all html collection in buttons 
+// what does button contains: nodelist(4) [span.button, span.button, span.button, span.button]
 const body=document.querySelector('body')
 buttons.forEach(function(b){
     b.addEventListener('click',function(e){
-        if(e.target.id===e.target.id){
-            body.style.backgroundColor=e.target.id
+        if(e.target.id==="grey"){
+            body.style.backgroundColor="grey"
+        }
+        else if(e.target.id==="white"){
+            body.style.backgroundColor="white"
+        }
+        else if(e.target.id==="blue"){
+            body.style.backgroundColor="blue"
+        }
+        else if(e.target.id==="yellow"){
+            body.style.backgroundColor="yellow"
         }
     })
 })
