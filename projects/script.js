3e30077d0c9cb0f4bb9a10a215d558daa0ba3fc0
Mayank Bisht <mayankbisht2112@gmail.com>
2026-01-26 12:27:36 +0530

    change

--- projects/script.js
+++ projects/script.js
@@ -2,8 +2,12 @@ const buttons=document.querySelectorAll('.button') // we are storing all html co
 // what does button contains: nodelist(4) [span.button, span.button, span.button, span.button]
 const body=document.querySelector('body')
 buttons.forEach(function(b){
+  //here on b a click event is used where when you click the button the function will be applied to it  
     b.addEventListener('click',function(e){
+// e is an event object here contains details about click 
         if(e.target.id==="grey"){
+//e.target is which element was clicked 
+//e.target.id is the id of the clicked element           
             body.style.backgroundColor="grey"
         }
         else if(e.target.id==="white"){
